dbef348c84d0f74507aa2165981e61b777b90e8f
Conner <conner0987@hotmail.com>
2017-01-05 18:58:47 -0600

    Update send.js

--- send.js
+++ send.js
@@ -3,7 +3,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 exports.handler = function(event, context) {
-    var messageText = var number = event.query.message;
+    var messageText = event.query.message;
     console.log(messageText);
   var sns = new AWS.SNS();
     var params = {
